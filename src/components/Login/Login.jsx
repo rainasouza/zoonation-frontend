c5c8d231d1f0279c808780c3fbf8f9856664d11a
rainasouza <raiina.py@gmail.com>
2023-05-17 22:18:38 -0300

    login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,17 +1,88 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {useState} from "react";
 import "bootstrap/dist/css/bootstrap.min.css"
 import { useNavigate } from "react-router-dom";
+import { useAuthetication } from "../../hooks/useAuthetication";
 
 
 
 function Login(){
+  const navigate = useNavigate();
+  const navigateToHome = () => {
+    navigate('/home');}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const {createUser, error: authError, loading} = useAuthetication(); 
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    setError("")
+
+    const user = {
+      email,
+      password
+    }
+
+
+   
+} 
+useEffect(() => {
+  if (authError){
+    setError(authError);
+  }
+}, [authError])
+
     return(
       <div>
-        <h1> Hello!</h1>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <a class="navbar-brand">ZooNation</a>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" onClick={navigateToHome} >Home</a>
+                </li>
+        
+              </ul>
+            </div>
+          </div>
+        </nav>
+        <br></br>
+
+        <div class="container">
+        <form class="row g-3" onSubmit={handleSubmit}>
+        <div class="col-sm-5">
+            <label>Email:</label>
+            <input type="email" class="form-control" name="email" required placeholder="Email do Usuário" value={email} onChange={(e) =>setEmail(e.target.value)}/>
+          </div>
+          <div class="col-sm-5">
+            <label>Senha:</label>
+            <input type="password" class="form-control" name="password" required placeholder="Mínimo de 6 Dígitos" value={password} onChange={(e) =>setPassword(e.target.value)}/>
+          </div>
+          
+          <div class="col-12">
+          {!loading && <button className="btn btn-outline-dark">Login</button>}
+          {loading && <button className="btn btn-outline-dark" disabled >Aguarde...</button>}
+          {error && <p className="error">{error}</p>}
+          </div>
+        </form>
+        </div>
+
+
+
+
       </div>
         
     )
 }
+  
+
+  
 
 export default Login;
